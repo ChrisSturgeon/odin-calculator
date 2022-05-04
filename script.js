524f3f7b45f17b1e059cb7c6c2a2a645b3669dbd
csturgeon <sturgeon.chris@gmail.com>
2022-05-04 16:26:33 +0100

    Addition works but bodged

--- script.js
+++ script.js
@@ -21,3 +21,62 @@ function divide(a, b) {
 function operate(operation, a, b) {
   return operation(a, b);
 }
+
+var displayValue = 0;
+var storedValue1 = 0;
+var operator;
+var storedValue2 = 0;
+
+
+// Clear inputs
+
+function clearDisplay() {
+  displayValue = 0;
+  storedValue1 = 0;
+  storedValue2 = 0;
+  operator = 0;
+  display.innerText = displayValue;
+}
+
+const display = document.getElementById("display");
+display.innerText = displayValue;
+
+function updateDisplay(input) {
+  console.log(input);
+  displayValue += input;
+  display.innerText = displayValue;
+}
+
+function selectOperator(input) {
+  operator = input;
+  storedValue1 = Number(displayValue);
+  console.log(`Selected operator: ${operator}`);
+  console.log(`Stored value 1: ${storedValue1}`);
+  displayValue = 0;
+  console.log(`Display Value now ${displayValue}`);
+}
+
+function greet() {
+  console.log("Hello")
+}
+
+function equals() {
+  storedValue2 = Number(displayValue);
+  if (operator === "add") {
+    console.log(operate(add, storedValue1, storedValue2));
+  }
+  console.log(`Stored value 1 : ${storedValue1}`)
+  console.log(`Stored value 2: ${storedValue2}`);
+  console.log(`Operator: ${typeof(operator)}`);
+  // console.log(operate(operator, storedValue1, storedValue2));
+
+}
+
+console.log(operate(add, 2 , 5));
+
+console.log(typeof(add(2 ,4)));
+
+
+
+
+
