69903c4ed7cd8215eae2a9bc91a7d287818eeff3
csturgeon <sturgeon.chris@gmail.com>
2022-05-04 11:38:15 +0100

    Rough layout complete

---
+++ script.js
@@ -0,0 +1,23 @@
+// Define operational functions
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Define operate function. 
+
+function operate(operation, a, b) {
+  return operation(a, b);
+}
