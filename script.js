6cdf04ceb64c9b5c8c3490226accc437fe38d0e3
csturgeon <sturgeon.chris@gmail.com>
2022-05-05 12:09:05 +0100

    Multiple operators working

--- script.js
+++ script.js
@@ -30,13 +30,6 @@ var inputArray = [];
 
 // Clear inputs
 
-function clearDisplay() {
-  displayValue = 0;
-  storedValue1 = 0;
-  storedValue2 = 0;
-  operator = 0;
-  display.innerText = displayValue;
-}
 
 const display = document.getElementById("display");
 display.innerText = displayValue;
@@ -46,50 +39,56 @@ function updateDisplay(input) {
   displayValue += input;
 }
 
-function saveValue1() {
-  storedValue1 = Number(displayValue);
+function addNumber() {
+  totalNumbers.push(Number(displayValue));
+  console.log(totalNumbers);
   displayValue = 0;
 }
 
 function selectAdd() {
-  operator = add;
-  saveValue1();
+  addNumber();
+  operators.push(add);
 }
 
 function selectSubtract() {
-  operator = subtract;
-  saveValue1();
+  addNumber();
+  operators.push(subtract);
 }
 
 function selectMultiply() {
-  operator = multiply;
-  saveValue1();
+  addNumber();
+  operators.push(multiply);
 }
+
+function selectDivide() {
+  addNumber();
+  operators.push(divide);
+}
+
+document.getElementById('equals').addEventListener('click', equals);
+document.getElementById('add').addEventListener('click', selectAdd);
+document.getElementById('subtract').addEventListener('click', selectSubtract);
+document.getElementById('multiply').addEventListener('click', selectMultiply);
+document.getElementById('divide').addEventListener('click', selectDivide);
+document.getElementById('execute').addEventListener('click', addNumber);
+
+var operators = [];
+var totalNumbers = [];
 
 function equals() {
+  addNumber();
+  for (let i = operators.length - 1 ; i >= 0; i--) {
+    var tempNumbers = [totalNumbers[0], totalNumbers[1]];
+    var tempOperator = operators[0];
+    var tempResult = tempNumbers.reduce(tempOperator);
+    operators.shift();
+    totalNumbers.splice(0, 2, tempResult);
+  }
+  console.log(totalNumbers[0]);
 
 }
 
-// document.getElementById('equals').addEventListener('click', equals);
-// document.getElementById('add').addEventListener('click', selectAdd);
-// document.getElementById('subtract').addEventListener('click', selectSubtract);
-// document.getElementById('multiply').addEventListener('click', selectMultiply);
-// document.getElementById('log').addEventListener('click', logArray);
-// document.getElementById('execute').addEventListener('click', execute);
 
-var operators = [divide, add, subtract];
-var totalNumbers = [27, 8, 43, 6];
-
-for (let i = operators.length - 1 ; i >= 0; i--) {
-
-var tempNumbers = [totalNumbers[0], totalNumbers[1]];
-var tempOperator = operators[0];
-var tempResult = tempNumbers.reduce(tempOperator);
-operators.shift();
-totalNumbers.splice(0, 2, tempResult);
-}
-
-console.log(totalNumbers[0]);
 
 
 
