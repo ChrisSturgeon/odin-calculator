2e557d128119805ed7c3b699d32791e210d34f72
csturgeon <sturgeon.chris@gmail.com>
2022-05-05 15:22:07 +0100

    Logic working. Ready for UI

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function operate(operation, a, b) {
   return operation(a, b);
 }
 
-var displayValue = 0;
+var displayValue = "";
 var storedValue1 = 0;
 var operator;
 var storedValue2 = 0;
@@ -30,19 +30,28 @@ var inputArray = [];
 
 // Clear inputs
 
+function clear() {
+  displayValue = "";
+  display.innerText = "0";
+  operators = [];
+  totalNumbers = [];
+  
+}
+
 
 const display = document.getElementById("display");
-display.innerText = displayValue;
+
 
 function updateDisplay(input) {
-  console.log(input);
   displayValue += input;
+  display.innerText = displayValue;
 }
 
 function addNumber() {
-  totalNumbers.push(Number(displayValue));
-  console.log(totalNumbers);
-  displayValue = 0;
+  if (displayValue.length != 0 && displayValue != "0") {
+    totalNumbers.push(Number(displayValue));
+    displayValue = "";
+  }
 }
 
 function selectAdd() {
@@ -65,27 +74,47 @@ function selectDivide() {
   operators.push(divide);
 }
 
+function invert() {
+  displayValue *= -1;
+  display.innerText = displayValue;
+}
+
+document.getElementById('clear').addEventListener('click', clear);
 document.getElementById('equals').addEventListener('click', equals);
 document.getElementById('add').addEventListener('click', selectAdd);
 document.getElementById('subtract').addEventListener('click', selectSubtract);
 document.getElementById('multiply').addEventListener('click', selectMultiply);
 document.getElementById('divide').addEventListener('click', selectDivide);
-document.getElementById('execute').addEventListener('click', addNumber);
+document.getElementById('invert').addEventListener('click', invert);
 
 var operators = [];
 var totalNumbers = [];
 
 function equals() {
   addNumber();
-  for (let i = operators.length - 1 ; i >= 0; i--) {
-    var tempNumbers = [totalNumbers[0], totalNumbers[1]];
-    var tempOperator = operators[0];
-    var tempResult = tempNumbers.reduce(tempOperator);
-    operators.shift();
-    totalNumbers.splice(0, 2, tempResult);
-  }
-  console.log(totalNumbers[0]);
 
+  console.log("--- Inputs ---")
+  console.log(`totalNumbers: ${totalNumbers}`);
+  console.log(`Operators: ${operators}`);
+  console.log(`Display Value: ${displayValue}`);
+  
+  if (totalNumbers.length - operators.length === 1) {
+
+    for (let i = operators.length - 1 ; i >= 0; i--) {
+      var tempNumbers = [totalNumbers[0], totalNumbers[1]];
+      var tempOperator = operators[0];
+      var tempResult = tempNumbers.reduce(tempOperator);
+      operators.shift();
+      totalNumbers.splice(0, 2, tempResult);
+      display.innerText = totalNumbers;
+    }
+    console.log("--- First ---")
+    console.log(`Answer is ${totalNumbers}`);
+    console.log(`totalNumbers: ${totalNumbers}`);
+    console.log(`Operators: ${operators}`);
+    console.log(`Display Value: ${displayValue}`);
+    
+  }
 }
 
 
